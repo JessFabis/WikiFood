3b9783eccc659f8f7df47bbdbee89bad81809368
J.Fabiola Juárez Z <jefajuza@hotmail.com>
2019-06-30 17:00:33 -0500

    se crearon los componentes faltantes y ya se asignaron las rutas ara el crrecto funcionamiento de la pagina , tambien es style se agrego algunas animaciones

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent } from "./components/home/home.component";
+import {AboutComponent } from "./components/about/about.component";
+import {RestaurantsComponent } from "./components/restaurants/restaurants.component";
+
+const APP_ROUTES: Routes = [
+    { path: 'home', component:HomeComponent },
+    { path: 'about', component:AboutComponent },
+    { path: 'restaurants', component:RestaurantsComponent },
+    { path: '**', pathMatch: 'full',redirectTo:'home'}
+
+
+];
+
+export const APP_ROUTING =RouterModule.forRoot(APP_ROUTES);
